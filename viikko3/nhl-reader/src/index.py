a743093756e09c65e1d2920783f5cef533ca127c
n0spoon <xtrifo@gmail.com>
2022-11-21 20:32:45 +0200

    viikko 3 tehtävä 1

---
+++ viikko3/nhl-reader/src/index.py
@@ -0,0 +1,30 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
+    response = requests.get(url).json()
+
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name'],
+            player_dict['nationality'],
+            player_dict['assists'],
+            player_dict['goals'],
+            player_dict['team']
+        )
+
+        players.append(player)
+
+    print("Oliot:")
+
+    for player in players:
+        print(player)
+
+if __name__ == "__main__":
+    main()
