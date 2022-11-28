76cd5eb6a6631cdd76a72487df91e702040fa7af
n0spoon <xtrifo@gmail.com>
2022-11-28 21:24:46 +0200

    Tehtävä 5.12 Kahden saman tuotteen lisäämisen jälkeen ostoskori sisältää ostoksen jolla sama nimi kuin tuotteella ja lukumäärä 2

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -82,3 +82,13 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(len(ostokset), 1)
         self.assertEqual(ostos.lukumaara(), 2)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskori_sisaltaa_ostoksen_jolla_sama_nimi_kuin_tuotteella_ja_lukumaara_kaksi(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        ostos = self.kori.ostokset()[0]
+        self.assertEqual(ostos.tuotteen_nimi(), 'Maito')
+        self.assertEqual(ostos.lukumaara(), 2)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
